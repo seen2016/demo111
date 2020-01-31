5503a8dad8f58162bff0cf6a30daf3a00497540d
lipengfei <lpf@xiyuan.com>
2020-01-31 16:01:25 +0800

    完成轮播组件 添加list属性

--- demo111/src/pages/home/components/HomeSwiper.vue
+++ demo111/src/pages/home/components/HomeSwiper.vue
@@ -1,26 +1,27 @@
 <template>
-  <div >
+  <div class="wrapper">
     <!--<h1>{{ msg }}</h1>-->
-    <swiper class="wrapper" :options="swiperOption" ref="mySwiper">
+    <swiper :options="swiperOption" ref="mySwiper" >
       <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img"
-          src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg"/>
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url"/>
       </swiper-slide>
-      <swiper-slide><img class="swiper-img"
-        src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg"/>
-      </swiper-slide>
-<!--      <swiper-slide>I'm Slide 3</swiper-slide>
-      <swiper-slide>I'm Slide 4</swiper-slide>-->
+<!--      <swiper-slide><img class="swiper-img"
+                         src="https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg"/>
+      </swiper-slide>-->
+      <!--      <swiper-slide>I'm Slide 3</swiper-slide>
+            <swiper-slide>I'm Slide 4</swiper-slide>-->
 
       <!-- Optional controls -->
       <div class="swiper-pagination" slot="pagination"></div>
       <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
       <!--<div class="swiper-button-next" slot="button-next"></div>-->
       <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+
     </swiper>
     <div>test</div>
   </div>
+
 </template>
 
 <script>
@@ -30,10 +31,19 @@
       return {
         msg: '轮播组件',
         swiperOption: {
-            pagination:'.swiper-pagination',
-            loop:true
-
-        }
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        swiperList: [
+          {
+            id: "01",
+            url: "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg"
+          },
+          {
+            id: "02",
+            url: "https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20201/be6871c9b0b0ffab3ef2a86fa9688677.jpg"
+          }
+        ]
 
       }
     }
@@ -43,9 +53,12 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" type="text/stylus" scoped>
   @import "~styles/varibles.styl" //引入全局资源文件
-  .wrapper >>> .swiper-pagination-bullet-active
+  .
+  wrapper > > >
+  .swiper-pagination-bullet-active
     background #fff
     margin: 0 4px
+
   .wrapper
     overflow: hidden
     width 100%
