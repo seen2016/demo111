cc675c2c581d9e61c3fd1912e76fe58266144ec3
lipengfei <lpf@xiyuan.com>
2020-01-31 14:04:39 +0800

    修改头部组件文件 创建轮播组件home-swiper分支

---
+++ demo111/src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'homeHeader',
+    data () {
+      return {
+        msg: '轮播组件'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" type="text/stylus" scoped>
+  @import "~styles/varibles.styl" //引入全局资源文件
+
+</style>
